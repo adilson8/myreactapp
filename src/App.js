5f3c5fedae3525d360f2f9103dc4904888fa2d52
adilson8 <adilson8@naver.com>
2025-01-24 20:41:37 +0900

    컴포넌트화 전전

--- src/App.js
+++ src/App.js
@@ -3,8 +3,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello React!
+    <div>
+      <header>
+        <h1>
+          <a href="/">WEB</a>
+        </h1>
+      </header>
+
+      <nav>
+        <ol>
+          <li>
+            <a href="/read/1">html</a>
+          </li>
+          <li>
+            <a href="/read/2">css</a>
+          </li>
+          <li>
+            <a href="/read/3">js</a>
+          </li>
+        </ol>        
+      </nav>
+
+      <article>
+        <h2>Welcome</h2>
+        Hello, WEB
+      </article>
     </div>
   );
 }
