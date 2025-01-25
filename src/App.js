18f18ffdcf9d6e9aa170e1b96992c38843d7e809
adilson8 <adilson8@naver.com>
2025-01-25 19:05:46 +0900

    컴포넌트화화

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header(){
+  return  <header>
+    <h1>
+      <a href="/">REACT</a>
+    </h1>
+  </header>
+}
+
+function Nav(){
+  return <nav>
+    <ol>
+      <li>
+        <a href="/read/1">html</a>
+      </li>
+      <li>
+        <a href="/read/2">css</a>
+      </li>
+      <li>
+        <a href="/read/3">js</a>
+      </li>
+    </ol>        
+  </nav>
+}
+
+function Article(){
+  return <article>
+    <h2>Welcome</h2>
+    Hello, WEB
+  </article>
+}
+
+
 function App() {
   return (
     <div>
-      <header>
-        <h1>
-          <a href="/">WEB</a>
-        </h1>
-      </header>
+      <Header></Header>
 
-      <nav>
-        <ol>
-          <li>
-            <a href="/read/1">html</a>
-          </li>
-          <li>
-            <a href="/read/2">css</a>
-          </li>
-          <li>
-            <a href="/read/3">js</a>
-          </li>
-        </ol>        
-      </nav>
-
-      <article>
-        <h2>Welcome</h2>
-        Hello, WEB
-      </article>
+      <Nav></Nav>
+      
+      <Article></Article>
     </div>
   );
 }
